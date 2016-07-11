e42dab1887bdbabe7c1bd32807d943eb583f4fd0
Niklas Lindström <lindstream@gmail.com>
2016-07-11 15:50:50 +0200

    Update PEG to 0.9.0

--- test/test_trig.js
+++ test/test_trig.js
@@ -1,10 +1,10 @@
-var fs = require('fs')
-var trigParser = require('../lib/parsers/trig')
+var fs = require('fs');
+var trigParser = require('../lib/parsers/trig');
 
 try {
-  var result = trigParser.parse(fs.readFileSync(__dirname + '/misc.trig', 'utf-8'))
-  console.log(JSON.stringify(result, null, 2))
+  var result = trigParser.parse(fs.readFileSync(__dirname + '/misc.trig', 'utf-8'));
+  console.log(JSON.stringify(result, null, 2));
 } catch (e) {
-  console.error(e.name, 'at', 'line:', e.line, 'column:', e.column, e.message)
+  console.error(JSON.stringify(e));
   throw e;
 }
