e459f8e419b52358919a242715d8cb9bbd427640
Niklas Lindström <lindstream@gmail.com>
2016-07-12 20:07:30 +0200

    Pass XMLSerializer directly to RDF/XML parser

--- test/test_rdfxml.js
+++ test/test_rdfxml.js
@@ -1,15 +1,11 @@
 var fs = require('fs');
 var rdfxmlParser = require('../lib/rdfxml/parser');
-var xmldom = require('xmldom'),
-  DOMParser = xmldom.DOMParser,
-  XMLSerializer = xmldom.XMLSerializer;
-var serializer = new XMLSerializer();
-rdfxmlParser.serialize = serializer.serializeToString.bind(serializer);
+var xmldom = require('xmldom');
 
-var rdfStr = fs.readFileSync(__dirname + '/misc.rdf', 'utf-8');
+var xmlStr = fs.readFileSync(__dirname + '/misc.rdf', 'utf-8');
 
-var doc = new DOMParser().parseFromString(rdfStr, 'text/xml');
-
-var result = rdfxmlParser.parse(doc);
+var result = rdfxmlParser.parse(
+  new xmldom.DOMParser().parseFromString(xmlStr, 'text/xml'),
+  new xmldom.XMLSerializer());
 
 console.log(JSON.stringify(result, null, 2));
