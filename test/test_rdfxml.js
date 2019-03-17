d113d684e8ff1f69eada3fa2723709f47775dc4f
Niklas Lindström <lindstream@gmail.com>
2019-03-17 09:39:16 +0100

    Change from using global to using compat module
    The compat module is self-initialized in browsers, but requires
    populating using setup on node right now. This isolates the use of
    require to the index entry point. (Which is desired since require isn't
    available in ES modules).
    
    Also use imports more consistently throughout.

--- test/test_rdfxml.js
+++ test/test_rdfxml.js
@@ -1,17 +1,14 @@
 const tape = require('tape')
 const fs = require('fs')
-const rdfxmlParser = require('../lib/rdfxml/parser')
-const xmldom = require('xmldom')
 
-global.XMLSerializer = xmldom.XMLSerializer
+require('../lib/setup')
+const rdfxmlParser = require('../lib/rdfxml/parser')
 
 let datadir = __dirname + '/data'
 
 tape.test('parse rdfxml', t => {
   let xmlStr = fs.readFileSync(datadir + '/misc.rdf', 'utf-8')
-  let result = rdfxmlParser.parse(
-    new xmldom.DOMParser().parseFromString(xmlStr, 'text/xml'),
-    new xmldom.XMLSerializer())
+  let result = rdfxmlParser.parse(xmlStr)
   let expected = JSON.parse(fs.readFileSync(datadir + '/rdfxml-1-out.jsonld', 'utf-8'))
   t.deepEqual(result, expected)
   t.end()
